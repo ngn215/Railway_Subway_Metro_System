c2d5973c09e3315b0328bb6c5696fb9f206c7b59
ngn215 <ngn215@nyu.edu>
2016-12-06 20:49:52 -0500

    Added code to generate persons for multiple lines
    Added code to generate persons for multiple lines (central and western).

--- src/Factory/PersonFactory.java
+++ src/Factory/PersonFactory.java
@@ -22,23 +22,39 @@ public class PersonFactory {
 		int i = 1;
 		Random rn = new Random();
 		PrintWriter writer = null;	
+		List<Station> westernLineStationsList = LineFactory.getLineInstance("WesternSlow").getStationsList();
+		List<Station> centralLineStationsList = LineFactory.getLineInstance("CentralSlow").getStationsList();
+		int listSize;
 		
+		boolean flip = true; //flag to decide whether we are adding person to werstern or central line
 		try
 		{
 		    writer = new PrintWriter("Logs/PersonsList.txt", "UTF-8");
 		    
 			while(i <= personsCount)
 			{
-				List<Station> westernLineStationsList = LineFactory.getLineInstance("WesternSlow").getStationsList();
-				int listSize = westernLineStationsList.size();
+				if (flip)
+					listSize = westernLineStationsList.size();
+				else
+					listSize = centralLineStationsList.size();
 				
 				int index1 = rn.nextInt(listSize);
 				int index2 = rn.nextInt(listSize);
 				
 				if (index1 != index2)
 				{
-					Station sourceStation = westernLineStationsList.get(index1);
-					Station destinationStation = westernLineStationsList.get(index2);
+					Station sourceStation, destinationStation;
+					
+					if (flip)
+					{
+						sourceStation = westernLineStationsList.get(index1);
+						destinationStation = westernLineStationsList.get(index2);
+					}
+					else
+					{
+						sourceStation = centralLineStationsList.get(index1);
+						destinationStation = centralLineStationsList.get(index2);
+					}
 					
 					Person person = new Person("P" + i, sourceStation, destinationStation);
 					sourceStation.enterStation(person);
@@ -58,6 +74,7 @@ public class PersonFactory {
 				}
 				
 				i++;
+				flip = !flip;
 			}
 		} //end try
 		catch (IOException e) 
