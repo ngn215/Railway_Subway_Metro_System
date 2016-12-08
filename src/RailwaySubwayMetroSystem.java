885e8163e65e60112bb52a9bb2ada6e78325c57d
ngn215 <ngn215@nyu.edu>
2016-12-07 22:06:41 -0500

    Made corresponding changes to Main class from previous commits.
    Made corresponding changes to Main class from previous commits.

--- src/RailwaySubwayMetroSystem.java
+++ src/RailwaySubwayMetroSystem.java
@@ -3,6 +3,7 @@ import Factory.LineFactory;
 import Factory.PersonFactory;
 import Factory.StationFactory;
 import Factory.TrainFactory;
+import Status.StationStatus;
 import Status.TrainStatus;
 
 public class RailwaySubwayMetroSystem {
@@ -13,21 +14,15 @@ public class RailwaySubwayMetroSystem {
 		//initialize station, line and person factory.
 		StationFactory.initializeFactory();
 		LineFactory.initializeFactory();
-		PersonFactory.randomlyGeneratePersons(15000);
+		PersonFactory.randomlyGeneratePersons(10000, "WesternSlow");
+		PersonFactory.randomlyGeneratePersons(10000, "CentralSlow");
+		PersonFactory.randomlyGeneratePersons(10000, "HarborPanvel");
+		PersonFactory.randomlyGeneratePersons(2000, "HarborAndheri");
 		
 		TrainFactory.getTrainInstance("W1", "WesternSlow", "Up", 1000, true);
-		TrainFactory.getTrainInstance("W2", "WesternFast", "Up", 1500, true);
-		
-		try {
-			Thread.sleep(3000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		TrainFactory.getTrainInstance("W3", "WesternSlow", "Down", 500, true);
+		TrainFactory.getTrainInstance("W2", "WesternFast", "Up", 500, true);
 		TrainFactory.getTrainInstance("C1", "CentralSlow", "Up", 1000, true);
-		TrainFactory.getTrainInstance("C2", "CentralFast", "Down", 1500, true);
+		TrainFactory.getTrainInstance("C2", "CentralFast", "Down", 100, true);
 		
 		try {
 			Thread.sleep(3000);
@@ -36,10 +31,24 @@ public class RailwaySubwayMetroSystem {
 			e.printStackTrace();
 		}
 		
-		TrainFactory.getTrainInstance("C3", "CentralSlow", "Up", 100, true);
+		TrainFactory.getTrainInstance("W3", "WesternSlow", "Down", 1500, true);
+		TrainFactory.getTrainInstance("C3", "CentralSlow", "Up", 1500, true);
 		
-		TrainStatus ts = new TrainStatus(TrainFactory.getTrainsList(), 60000);
-		ts.getTrainsStatus();
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		TrainFactory.getTrainInstance("W3", "HarborPanvel", "Down", 1500, true);
+		TrainFactory.getTrainInstance("C3", "HarborAndheri", "Up", 1500, true);
+		
+		TrainStatus trainStatus = new TrainStatus(TrainFactory.getTrainsList(), 60000);
+		trainStatus.getTrainsStatus();
+		
+		//StationStatus stationStatus = new StationStatus(StationFactory.getStationsList(), 60000);
+		//stationStatus.getStationsStatus();
 		
 		//Intersection westernCentralIntersection = new Intersection("Western-Central1", westernSlowLine, centralSlowLine, "Dadar");
 	}
