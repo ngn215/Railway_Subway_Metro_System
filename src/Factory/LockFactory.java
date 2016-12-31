cfc858498b90479b1b0728c39c6df32d49e210ca
ngn215 <ngn215@nyu.edu>
2016-12-31 12:42:05 -0500

    created new lock factory

---
+++ src/Factory/LockFactory.java
@@ -0,0 +1,25 @@
+package Factory;
+
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+
+public class LockFactory {
+	
+	private static int lockCount = 0;
+
+	public static ReentrantReadWriteLock getReentrantReadWriteLockInstance(boolean fair)
+	{
+		incrementLockCount();
+		return new ReentrantReadWriteLock(fair);
+	}
+	
+	private static void incrementLockCount()
+	{
+		lockCount++;
+	}
+	
+	public int getLockCount()
+	{
+		return lockCount;
+	}
+}
